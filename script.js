9121b420b60f3b3991b55cda08c25fb9baadd618
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-30 16:45:43 +0000

    added form and can type in data

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ function handleSubmit() {
 	// 	console.log(this.responseText);
 	// };
 	// var url = 'https://example.com/profile';
-	var data = { username: 'andrew' };
+	// var data = { username: 'andrew' };
 
-	// const data = {
-	// 	"username": document.getElementById("username").value,
-	// 	"password": document.getElementById("password").value
-	// };
+	const data = {
+		"username": document.getElementById("username").value,
+		"password": document.getElementById("password").value
+	};
 	fetch("https://cors-anywhere.herokuapp.com/http://careerdev-teaching-lujason2015.c9users.io/api", {
 			method: 'POST',
 			body: JSON.stringify(data),
@@ -22,16 +22,16 @@ function handleSubmit() {
 			}
 
 		})
-		// .then(res => res.json())
-		// .then(response => console.log('Success:', JSON.stringify(response)))
-		// .catch(error => console.error('Error:', error));
+		.then(res => res.json())
+		.then(response => console.log('Success:', JSON.stringify(response)))
+		.catch(error => console.error('Error:', error));
 
-	.then(function(response) {
-		return response.json();
-	})
-	.then(function(myJson) {
-		console.log(JSON.stringify(myJson));
-	});
+	// .then(function(response) {
+	// 	return response.json();
+	// })
+	// .then(function(myJson) {
+	// 	console.log(JSON.stringify(myJson));
+	// });
 
 	// xhttp.open("POST", "https://cors-anywhere.herokuapp.com/http://careerdev-teaching-lujason2015.c9users.io/api", true);
 	// xhttp.setRequestHeader("Content-type", "application/json");
