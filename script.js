0465d56fb36ff174d7c8c2901fdcb38c6cbf2f02
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-30 16:44:26 +0000

    posting w/o form and hardcoded data

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function handleSubmit() {
 	// xhttp.onload = function() {
 	// 	console.log(this.responseText);
 	// };
-	var url = 'https://example.com/profile';
-	// var data = { username: 'andrew' };
+	// var url = 'https://example.com/profile';
+	var data = { username: 'andrew' };
 
-	const data = {
-		"username": document.getElementById("username").value,
-		"password": document.getElementById("password").value
-	};
+	// const data = {
+	// 	"username": document.getElementById("username").value,
+	// 	"password": document.getElementById("password").value
+	// };
 	fetch("https://cors-anywhere.herokuapp.com/http://careerdev-teaching-lujason2015.c9users.io/api", {
 			method: 'POST',
 			body: JSON.stringify(data),
@@ -21,16 +21,17 @@ function handleSubmit() {
 				'Content-Type': 'application/json'
 			}
 
-		}).then(res => res.json())
-		.then(response => console.log('Success:', JSON.stringify(response)))
-		.catch(error => console.error('Error:', error));
+		})
+		// .then(res => res.json())
+		// .then(response => console.log('Success:', JSON.stringify(response)))
+		// .catch(error => console.error('Error:', error));
 
-	// .then(function(response) {
-	// 	return response.json();
-	// })
-	// .then(function(myJson) {
-	// 	console.log(JSON.stringify(myJson));
-	// });
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function(myJson) {
+		console.log(JSON.stringify(myJson));
+	});
 
 	// xhttp.open("POST", "https://cors-anywhere.herokuapp.com/http://careerdev-teaching-lujason2015.c9users.io/api", true);
 	// xhttp.setRequestHeader("Content-type", "application/json");
